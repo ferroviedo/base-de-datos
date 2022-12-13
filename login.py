c6f7d1d1a9515ee9ac76ef5e806c15dc45d4f208
feroviedo16 <108472752+feroviedo16@users.noreply.github.com>
2022-12-13 14:58:12 -0300

    nuevos cambios

--- login.py
+++ login.py
@@ -3,32 +3,36 @@ import tkinter as tk
 import sqlite3
 from tkinter import *
 from tkinter import ttk
-#cursor = conexion.cursor()
-#cursor.execute
 ventana = tkinter.Tk()
 ventana.title ("Login")
 ventana.geometry ("450x250+500+250")
 ventana.config(bg = "skyblue3")
 boton = ttk.Button(text="Registrarte")
-boton.place(x=185, y=195)
+boton.place(x=185, y=175)
 ventana.resizable(0, 0)
-Label(ventana, text = "Nombre de Usuario:").pack()
-caja1 = Entry(ventana)
+Label(ventana, text = "Usuario:").pack()
+caja1 = Entry(ventana, bg = "lightblue")
 caja1.pack()
 Label(ventana, text = "Contraseña:").pack()
-caja2 = Entry(ventana, show = "*")
+caja2 = Entry(ventana,  bg = "lightblue",show = "*")
 caja2.pack()
-Label(ventana, text = "Numero de telefono:").pack()
-caja3 = Entry(ventana)
+Label(ventana, text = "Telefono:").pack()
+caja3 = Entry(ventana, bg = "lightblue")
 caja3.pack()
 Label(ventana, text = "Dni:").pack()
-caja4 = Entry(ventana)
+caja4 = Entry(ventana, bg = "lightblue")
 caja4.pack()
 
+bd = sqlite3.connect("login.py")
+cursor =  bd.cursor()
+cursor.execute("SELECT * FROM Datos")
+print(cursor)
+datos = fetchone()
+print(datos)
+bd.commit()
 
-def Login(self):
-    bd = sqlite3.connect("/Carpeta Personal/Documentos/Login.bd")
-    c = db.cursor()
-    
+cursor.execute("INSERT INTO Datos VALUES " \
+            "('fer',12345,'1122334455','9090909')")
+
     
 ventana.mainloop()
