8751c405617402ae1502ba06324ecdf70dbb1200
feroviedo16 <108472752+feroviedo16@users.noreply.github.com>
2022-12-16 11:05:18 -0300

    nuevas funciones

--- login.py
+++ login.py
@@ -3,36 +3,40 @@ import tkinter as tk
 import sqlite3
 from tkinter import *
 from tkinter import ttk
+
+def escribir():  
+    nombre = caja1.get()
+    apellido = caja2.get()
+    dni = caja3.get()
+    print(nombre,apellido,dni)
+    
+def leer():
+    respuesta.get(*)
+    
 ventana = tkinter.Tk()
 ventana.title ("Login")
 ventana.geometry ("450x250+500+250")
 ventana.config(bg = "skyblue3")
-boton = ttk.Button(text="Registrarte")
-boton.place(x=185, y=175)
+boton = ttk.Button(text="Guardar", command = escribir)
+boton.place(x=245, y=150)
+boton = ttk.Button(text="Buscar")
+boton.place(x=110, y=150)
 ventana.resizable(0, 0)
-Label(ventana, text = "Usuario:").pack()
+Label(ventana, text = "Nombre:", bg = "skyblue3").pack()
 caja1 = Entry(ventana, bg = "lightblue")
 caja1.pack()
-Label(ventana, text = "Contraseña:").pack()
-caja2 = Entry(ventana,  bg = "lightblue",show = "*")
+Label(ventana, text = "Apellido:", bg ="skyblue3").pack()
+caja2 = Entry(ventana,  bg = "lightblue")
 caja2.pack()
-Label(ventana, text = "Telefono:").pack()
+Label(ventana, text = "Dni:", bg = "skyblue3").pack()
 caja3 = Entry(ventana, bg = "lightblue")
 caja3.pack()
-Label(ventana, text = "Dni:").pack()
-caja4 = Entry(ventana, bg = "lightblue")
-caja4.pack()
 
-bd = sqlite3.connect("login.py")
-cursor =  bd.cursor()
-cursor.execute("SELECT * FROM Datos")
-print(cursor)
-datos = fetchone()
-print(datos)
-bd.commit()
+lista=["0", "1", "2",]
+combo = ttk.Combobox(values=lista)
+combo.place(x=130, y=190)
 
-cursor.execute("INSERT INTO Datos VALUES " \
-            "('fer',12345,'1122334455','9090909')")
+    
 
     
 ventana.mainloop()
